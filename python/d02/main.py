a6f8c8c22bdce5b6dd5d64004cb6f9c6dcdba5c1
Amar1729 <amar.paul16@gmail.com>
2022-12-02 00:12:29 -0500

    d2p2

--- python/d02/main.py
+++ python/d02/main.py
@@ -75,15 +75,67 @@ def day1(data) -> int:
 
 
 def day2(data) -> int:
-    total = 0
+    score = 0
 
-    # todo
+    for line in data:
+        opponent, player = line.split(" ")
 
-    return total
+        # need to lose
+        if player == "X":
+            score += 0
+
+            # rock
+            if opponent == "A":
+                # scissors
+                score += 3
+            # paper
+            elif opponent == "B":
+                # rock
+                score += 1
+            # scissors
+            elif opponent == "C":
+                # paper
+                score += 2
+
+        # need to draw
+        elif player == "Y":
+            score += 3
+
+            # rock
+            if opponent == "A":
+                # rock
+                score += 1
+            # paper
+            elif opponent == "B":
+                # paper
+                score += 2
+            # scissors
+            elif opponent == "C":
+                # scissors
+                score += 3
+
+        # need to win
+        elif player == "Z":
+            score += 6
+
+            # rock
+            if opponent == "A":
+                # paper
+                score += 2
+            # paper
+            elif opponent == "B":
+                # scissors
+                score += 3
+            # scissors
+            elif opponent == "C":
+                # rock
+                score += 1
+
+    return score
 
 
 if __name__ == "__main__":
     data = parse(sys.argv[1])
 
-    print(day1(data))
+    # print(day1(data))
     print(day2(data))
