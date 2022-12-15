056ee43eaa37d90e06df211666989bd0762fd9d2
Amar1729 <amar.paul16@gmail.com>
2022-12-15 03:31:43 -0500

    d15 switch DisjointSet over to Dict internally (how to make fast?)

--- python/d15/main.py
+++ python/d15/main.py
@@ -46,7 +46,7 @@ def manhattan_range(s: Point, b: Point):
 
 class DisjointSet:
     def __init__(self):
-        self.ranges = []
+        self.ranges = {}
 
     def __add__(self, tup):
         self.update(*tup)
@@ -54,47 +54,50 @@ class DisjointSet:
         d.ranges = self.ranges
         return d
 
+    def __contains__(self, i: int) -> bool:
+        # TODO: maybe?
+        return False
+
     def update(self, i: int, f: int):
-        # somewhat gross impl, sorry
-        # maybe this should use a Dict[start: int, stop: int] ?
-
-        for c in range(len(self.ranges)):
-
-            ri, rf = self.ranges[c]
+        for ri, rf in sorted(self.ranges.items(), key=lambda p: p[0]):
 
             if i > rf + 1:
                 # try next-highest
                 continue
             elif ri <= i <= rf + 1:
-                self.ranges.pop(c)
+                # update starts inside or adjacent to existing range
+                del self.ranges[ri]
                 self.update(ri, max(rf, f))
                 return
             elif ri - 1 <= f <= rf:
-                self.ranges.pop(c)
+                # update finishes in or adjacent to existing range
+                del self.ranges[ri]
                 self.update(min(ri, i), rf)
                 return
             elif i <= ri and f >= rf:
-                self.ranges.pop(c)
+                # update is superset of existing range
+                del self.ranges[ri]
                 self.update(i, f)
                 return
             elif f < ri - 1:
-                self.ranges.insert(c, (i, f))
+                # update is wholly less than existing range
+                self.ranges[i] = f
                 return
             else:
                 # there are no other possibilities, raise exception
                 raise Exception
 
-        self.ranges.append((i, f))
+        self.ranges[i] = f
 
     def __repr__(self) -> str:
         # dbg
-        return "\n".join([f"<{i}, {f}>" for i, f in self.ranges])
+        return "\n".join([f"<{i}, {f}>" for i, f in self.ranges.items()])
 
     def __str__(self) -> str:
         # dbg
-        length = len(self.ranges)
-        i = self.ranges[0][0]
-        f = self.ranges[-1][1]
+        length = len(self.ranges.keys())
+        i = min(self.ranges.keys())
+        f = max(self.ranges.values())
         return f"{length} splits: <{i}, {f}>"
 
 
@@ -107,6 +110,8 @@ def part1(data) -> int:
     target = 2_000_000
 
     for sensor, beacon in data.items():
+        # print(sensor, beacon)
+
         for xi, xf, y in manhattan_range(sensor, beacon):
             # look only at the points our problem wants?
             if y != target:
@@ -118,7 +123,7 @@ def part1(data) -> int:
     actual_points = set()
     beacons = set(data.values())
 
-    for i, f in blocked[target].ranges:
+    for i, f in blocked[target].ranges.items():
         for x in range(i, f + 1):
             p = Point(x, target)
             if p not in data and p not in beacons:
@@ -135,6 +140,8 @@ def part2(data) -> int:
     m = 4_000_000
 
     for sensor, beacon in data.items():
+        # print(sensor, beacon)
+
         # will this help with efficiency during initial calc?
         for dev in (sensor, beacon):
             if dev.x in range(0, m + 1) and dev.y in range(0, m + 1):
@@ -152,8 +159,8 @@ def part2(data) -> int:
             blocked[y] += (xi, xf)
 
     for y, ds in blocked.items():
-        if len(ds.ranges) > 1:
-            return y + (ds.ranges[0][1] + 1) * m
+        if len(ds.ranges.keys()) == 2:
+            return y + (min(ds.ranges.values()) + 1) * 4_000_000
 
     # none found
     raise Exception
@@ -163,5 +170,5 @@ if __name__ == "__main__":
     data = parse(sys.argv[1])
     devices = parse_devices(data)
 
-    # print(part1(devices))
+    print(part1(devices))
     print(part2(devices))
